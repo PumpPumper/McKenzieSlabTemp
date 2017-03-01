01db49de0fef861df4db33e7070c11a6a6dd4480
Joe Rippke <rippk003@umn.edu>
2017-03-01 17:41:15 -0600

    I tinkered around with it for a while and I can't remember exactly what I changed, but it works.

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ Created on Wed Feb 22 14:50:55 2017
 
 This module exists to run the McKenzie sinking slab thermal structure model
 """
-import slabcalc as sc
+import slabcalc as sc # module that contains the model
+#import lithosphere as li # module that contains a variation of the model that I'm playing around with
 
 # Set Values
 
@@ -16,7 +17,8 @@ Cp = 0.35 # cal/(g °C)         set value of heat capacity
 vx = 10 # cm/yr                set value of slab velocity
 l = 50 # km                    set value of slab thickness
 kappa = 0.01 # cal/(cm °C s)   set value of thermal conductivity
-xmax = 1000 # km               set maximum slab length (arbitrary)
+xmax = 1000 # km               set maximum slab length
 dip = 20 # °                   set slab dip angle
 
 sc.slabmodel(xmax,l,rho,Cp,vx,kappa,dip)
+#li.slabmodel(xmax,l,rho,Cp,vx,kappa,dip)
